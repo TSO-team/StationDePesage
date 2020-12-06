72c34ed4d2a55a02c81a185bea386142fe1590be
Samuel Duclos <wiseakasha@hotmail.com>
2020-12-06 14:18:36 -0500

    Update share_internet.sh

--- bash/internet/share_internet.sh
+++ bash/internet/share_internet.sh
@@ -5,13 +5,13 @@
 # For:         My team.
 # Description: Share internet through existing wired connection from a
 #              Linux computer to a Linux device without internet access.
-# Usage:       sudo bash utils/internet/share_internet.sh <OUT> <IN>
-# Example:     sudo bash utils/internet/share_internet.sh wlan0 eth0
+# Usage:       sudo bash bash/internet/share_internet.sh <OUT> <IN>
+# Example:     sudo bash bash/internet/share_internet.sh wlan0 eth0
 # Arguments:   <OUT>: interface with access to internet    (default is "wlan0")
 #              <IN>:  interface without access to internet (default is "eth0")
 # Note:        Repeat at each reboot.
 
-# Parse and set arguments from command-line.
+# Parse and set optional arguments from command-line.
 OUT=$(echo "${1:-wlan0}" | tr '[:upper:]' '[:lower:]')
 IN=$(echo "${2:-eth0}" | tr '[:upper:]' '[:lower:]')
 
