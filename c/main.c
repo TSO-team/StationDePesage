09664620b860a3c95d159274d635fa32e3620431
Samuel Duclos <nomfullcreatif@gmail.com>
2020-12-07 08:28:45 -0500

    Writing main.c.

--- c/main.c
+++ c/main.c
@@ -5,71 +5,116 @@
 //              - finish Python (see Python)
 //              - translate from Python
 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <string.h>
 #include <unistd.h>
 
 #define SHELL "/bin/bash"
 
-void cleanup(void);
+static pid_t return_value; // Modify for execl() in child.
+
+static void cleanup(int signo) {
+    fprintf(stderr, "Termination signal received (%d)! Cleaning up!\n", signo);
+    fprintf(stderr, "Killing child!\n");
+    kill(pid, SIGTERM);
+    sleep(3);
+}
 
 int main(int argc, char *argv[]) {
     const char *command = "/usr/bin/python3 /home/debian/workspace/StationDePesage/python/uARM_payload.py";
-    int status;
-    pid_t pid;
+    int pipefd[2], status;
+    pid_t ppid = getpid(), pid;
     char CAN_input;
+    bool is_breaking = false;
 
-    while (1) {
-        pid = fork();
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-        is_factory_reset = getchar(); // Simulating factory reset.
+    if (pipe(pipefd) == -1) {
+        fputs("PIPE ERROR!", stderr);
+        exit(EXIT_FAILURE);
+    }
 
-        if (is_factory_reset == 'y') {
-            while (1) {
-                CAN_input = getchar(); // CAN input goes here.
+    return_value = fork();
+    pid = getpid();
 
-                if (pid < 0) { // error
-                    fputs("FORK ERROR!", stderr);
-                    status = -1;
-                } else if (pid == 0) { // child
-                    signal(SIGHUP, cleanup);
-                    signal(SIGINT, cleanup);
-                    signal(SIGTERM, cleanup);
-                    execl(SHELL, SHELL, "-c", command, NULL);
-                    _exit(EXIT_FAILURE); // Fun fact: Avoids flushing fully-buffered streams like STDOUT.
-                } else { // parent
-                    if (CAN_input == 'k') {
-                        break;
-                    } else if (CAN_input == 'l') {
-                        if (waitpid(pid, &status, 0) != pid) {
-                            status = 1;
-                        }
-                    } else {
-                        fputs("PROTOCOL ERROR!", stderr);
-                    }
-                }
+    if (return_value == -1) { // error
+        fputs("FORK ERROR!", stderr);
+        status = -1;
+        //exit(EXIT_FAILURE);
 
-                puts(status); // CAN output goes there.
-            }
-        } else {
-            cleanup();
-            break;
+    } else if (!return_value) { // child
+        if (signal(SIGTERM, cleanup) == SIG_ERR) {
+            fputs("Error while setting SIGTERM signal handler!", stderr);
+            return EXIT_FAILURE;
         }
 
-        cleanup();
+        if (signal(SIGINT, cleanup) == SIG_ERR) {
+            fputs("Error while setting SIGINT signal handler!", stderr);
+            return EXIT_FAILURE;
+        }
+
+        if (signal(SIGHUP, cleanup) == SIG_ERR) {
+            fputs("Error while setting SIGHUP signal handler!", stderr);
+            return EXIT_FAILURE;
+        }
+
+        //execl(SHELL, SHELL, "-c", command, NULL);
+        //_exit(EXIT_FAILURE); // Fun fact: Avoids flushing fully-buffered streams like STDOUT.
+
+        close(pipefd[1]); // Close unused write end.
+        while (read(pipefd[0], &buf, 1) > 0) {
+            write(STDOUT_FILENO, &buf, 1);
+        }
+
+        write(STDOUT_FILENO, "\n", 1);
+        close(pipefd[0]);
+        _exit(EXIT_SUCCESS);
+
+    } else { // parent
+        close(pipefd[0]); // Close unused read end.
+        write(pipefd[1], argv[1], strlen(argv[1]));
+
+        while (1) {
+            is_factory_reset = getchar(); // Simulating factory reset.
+
+            if (is_factory_reset == 'y') {
+                while (1) {
+                    CAN_input = getchar(); // CAN input goes here.
+
+                    if (CAN_input == 'k') is_breaking = true;
+                    else if (CAN_input == 'l') {
+                        puts("Parent continues normally, everyone is happy!");
+                    } else {
+                        fputs("PROTOCOL ERROR!", stderr);
+                        is_breaking = true;
+                    }
+
+                    puts(status); // CAN output goes there.
+
+                    if (is_breaking) break;
+            } else break;
+        }
+
+        close(pipefd[1]); // Reader will see EOF.
+        //if (waitpid(pid, &status, 0) != pid) {
+        //   status = 1;
+        //}
+        wait(NULL); // Wait for child.
+        exit(EXIT_SUCCESS);
+    }
+
+    if (raise(SIGINT)) {
+        fputs("Error while raising SIGINT signal!", stderr);
+        return EXIT_FAILURE;
     }
 
     exit(EXIT_SUCCESS);
-}
-
-void cleanup(void) {
-    puts("Termination signal received! (do some complex cleanup...)");
-    fputs("Killing child!", stderr);
-    kill(pid, SIGTERM);
-    sleep(3);
-    exit(0);
 }
--- c/utils/uarm_scan_test.c
+++ c/utils/uarm_scan_test.c
@@ -14,7 +14,27 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-// Ewww... Update this using new Python version.
+// Ewww... Update this using new Python version here:
+/*
+    def scan(self, position=None, sensor=None, sensor_threshold=0.5):
+        corner_x = int(position['x'] - self.initial_position['x'])
+        corner_y = int(position['y'] - self.initial_position['y'])
+        corner_z = int(position['z'] - self.initial_position['z'])
+        is_breaking = False
+
+        if not uarm.grab(grab_position=position, sensor=sensor, sensor_threshold=sensor_threshold):
+            for k in range(corner_z, self.scan_z_displacement, self.stride_z):
+                for j in range(corner_y, self.scan_y_displacement, self.stride_y):
+                    for i in range(corner_x, self.scan_x_displacement, self.stride_x):
+                        scan_position = {'x': i, 'y': j, 'z': k, 'speed': self.initial_position['speed'], 'relative': False, 'wait': True}
+                        if uarm.grab(grab_position=scan_position, sensor=sensor, sensor_threshold=sensor_threshold):
+                            is_breaking = True
+                            break
+                    if is_breaking:
+                        break
+                if is_breaking:
+                    break
+*/
 void uarm_scan_for_object(void) {
     /*
     for (unsigned char m = 0; m < 128; m++) {
